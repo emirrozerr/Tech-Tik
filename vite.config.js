82aa53e0e36dec42db87a82aa4ecc236f54dfdb1
emirrozerr <emirrozerr@hotmail.com>
2022-12-26 17:48:29 +0300

    vite.config updated

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue2Jsx from '@vitejs/plugin-vue2-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tek-tik/',
+  base: '/Tek-Tik/',
   plugins: [
     vue2(),
     vue2Jsx(),
