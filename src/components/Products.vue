8a7bae9042fc4aacb1f60a36e36a911954245e73
emirrozerr <emirrozerr@hotmail.com>
2022-12-26 05:28:15 +0300

    products added

---
+++ src/components/Products.vue
@@ -0,0 +1,81 @@
+<script>
+
+</script>
+
+<template>
+    <section id="home-products-section">
+            <div class="container home-products-section-container">
+                <div class="home-products-section-text">
+                    <h2>Our <span>Products</span></h2>
+                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected randomised words which don't look even slightly believable</p>
+                </div>
+            </div>
+            <div class="home-products">
+                    <div class="container home-products-card">
+
+                        <b-card
+                        v-for="product in this.data.urunler" :key="product.urunID" 
+                        style="max-width: 21rem;min-height:400px;" class="pb-3 border-0 shadow"
+                        >
+                        <b-card-img
+                        :src="product.urunFotograflari[0]"
+                        >
+                        </b-card-img>
+                        <b-card-title class="text-center fs-3">
+                        {{product.urunAdi}}
+                        </b-card-title>
+                            <div class="d-flex justify-content-center flex-column align-items-center">
+                                <span class="fs-5 mt-2 mb-4 text-muted">{{ USDollar.format(product.urunFiyati) }}₺</span>
+                                <b-button href="#" variant="outline-success" class="rounded-4 p-2">Ürüne git</b-button>
+                            </div>
+                        </b-card>
+
+                    
+                    </div>
+            </div>
+    </section>
+</template>
+<script>
+import Vue from "vue"
+export default {
+    data() {
+      return {
+        data: [],
+        USDollar: new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            imgsrc: null,
+        }),
+      }
+    },
+    methods: {
+        async getData() {
+        const res = await fetch("http://139.59.156.77/test.json");
+        const finalRes = await res.json();
+        this.data = finalRes;
+        console.log(this.data)
+      },
+    },
+    created(){
+        this.getData()
+    }
+}
+
+
+</script>
+<style scoped>
+p{
+    margin-top: 20px;
+    color: rgb(0, 0, 0);
+    font-size: 1.2rem;
+    line-height: 2rem;
+}
+h2{
+    font-weight: 700 !important;
+    font-size: 3rem;
+}
+h2 span{
+    color: #136AF8;
+    
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../components/Login.vue'
 import SignUp from '../components/SignUp.vue'
+import Products from '../components/Products.vue'
 
 Vue.use(VueRouter)
 
@@ -10,14 +11,6 @@ const router = new VueRouter({
   mode: 'history',
   base: import.meta.env.BASE_URL,
   routes: [
-    {
-      path: '/home',
-      name: 'home',
-      component: Home,
-      meta:{
-        title:'Home'
-      }
-    },
     {
       path: '/',
       name: 'Login',
@@ -27,11 +20,27 @@ const router = new VueRouter({
       }
     },
     {
+      path: '/home',
+      name: 'home',
+      component: Home,
+      meta:{
+        title:'Home'
+      }
+    },
+    {
       path: '/signup',
       name: 'SignUp',
       component: SignUp,
       meta:{
         title:'SignUp'
+      }
+    },
+    {
+      path: '/products',
+      name: 'Products',
+      component: Products,
+      meta:{
+        title:'Products'
       }
     },
   ]
